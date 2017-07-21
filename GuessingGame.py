6f98a038f493b5c1277c59a5c513ec3f6d424a2d
joeldick <joeldic@gmail.com>
2017-07-21 00:41:49 -0400

    Initial Commit

---
+++ GuessingGame.py
@@ -0,0 +1,13 @@
+import random
+
+if __name__ == "__main__":
+    print("Welcome to GuessingGame v0.1")
+    playAgain = input("Do you want to play a guessing game? (Y/N): ")
+    while playAgain == "Y" or playAgain == "y":
+        print("I am thinking of a number between 1 and 10.")
+        computerNumber = str(random.randint(1,10))
+        guess = input("Try to guess my number: ")
+        while guess != computerNumber:
+            guess = input("Wrong! Guess again: ")
+        print("Yes! My number was " + computerNumber + "!")
+        playAgain = input("Play again? (Y/N): ")
